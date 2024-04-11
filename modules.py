d0516d386b194232c2e14e880ce5225dc5f133e2
gta-mas <gabriel.tamas11@proton.me>
2024-04-11 18:11:09 +0200

    simple module import

---
+++ modules.py
@@ -0,0 +1,9 @@
+# module = a file containing python code, may contain functions, classes, etc.
+# used with modular programming, which is to separate a program into parts
+
+import messages as msg        # creating an alias
+# from messages import hello,bye  # another way of importing
+# from messages import *          # * = imports ALL functions -> dangerous!
+
+msg.hello()
+msg.bye()
