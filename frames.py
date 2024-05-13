08ff2fec78cce56e0dad5102aa36f01bdf185a66
gta-mas <gabriel.tamas11@proton.me>
2024-05-13 17:14:48 +0200

    creating a simple frame

---
+++ frames.py
@@ -0,0 +1,21 @@
+# frame = rectangular container to group and hold widgets
+
+from tkinter import *
+
+window = Tk()
+
+frame = Frame(window,
+              bg="pink",
+              bd=5,
+              relief=SUNKEN)
+frame.place(x=0, y=0) # can be used instead of pack()
+
+Button(frame, text="W", font=("Consolas", 25), width=3).pack(side=TOP)
+Button(frame, text="A", font=("Consolas", 25), width=3).pack(side=LEFT)
+Button(frame, text="S", font=("Consolas", 25), width=3).pack(side=LEFT)
+Button(frame, text="D", font=("Consolas", 25), width=3).pack(side=LEFT)
+
+
+window.mainloop()
+
+
